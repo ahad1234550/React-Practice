f4257c8f5d80ce5c6a4af75266454dc0243f61ea
Ahad Danish <ahad@Ahads-MacBook-Pro.local>
2025-12-16 11:52:56 +0500

    Dynamic values in template

--- ahad-blog/src/App.js
+++ ahad-blog/src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
 
 function App() {
+  const title = "Welcome to new blog";
+  const likes = 50;
+  // const person = { name: 'ahad', age: 22 };
+
+  const link = "https://www.google.com";
   return (
     <div className="App">
       <div className="content">
-        <h1>App Components</h1>
+        <h1>{title}</h1>
+        <p>Liked {likes} times</p>
+        {/* <p> { person } </p> */}
+
+        <p>{"Hello Ninjas"}</p>
+        <p>{10}</p>
+        <p>{[10, 20, 30, 40]}</p>
+        <p>{Math.random() * 10}</p>
+
+        <a href={link}>Google</a>
       </div>
     </div>
   );
