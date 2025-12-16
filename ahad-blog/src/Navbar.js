90aabf5b78ebad938144ab80b62838e3af3efe0f
Ahad Danish <ahad@Ahads-MacBook-Pro.local>
2025-12-16 19:07:41 +0500

    Added Links for home and create in Navbar.js instead of a tag so that it do not make request when another link is clicked

--- ahad-blog/src/Navbar.js
+++ ahad-blog/src/Navbar.js
@@ -1,16 +1,12 @@
+import { Link } from 'react-router-dom';
+
 const Navbar = () => {
     return ( 
         <nav className="navbar">
             <h1>The Ahad's Blog</h1>
             <div className="links">
-                <a href="/">Home</a>
-                <a href="/create">New Blog</a>
-                {/* Inline Styling Example: first {} is for dynamic JS, second {} is for object
-                <a href="/create" style={{
-                    color: "white",
-                    backgroundColor: "#f1356d",
-                    borderRadius: "8px"
-                }}>New Blog</a> */}
+                <Link to="/">Home</Link>
+                <Link to="/create">New Blog</Link>
             </div>
         </nav>
      );
