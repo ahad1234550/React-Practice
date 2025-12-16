382f1920293facf2bb2f8a8175b535cf98e2aeb3
Ahad Danish <ahad@Ahads-MacBook-Pro.local>
2025-12-16 17:56:58 +0500

    Added error handling while fetching data through api

--- ahad-blog/src/Home.js
+++ ahad-blog/src/Home.js
@@ -5,15 +5,24 @@ const Home = () => {
 
     const [blogs, setBlogs] = useState(null);
     const [pending, setPending] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         fetch("http://localhost:8000/blogs")
             .then(res => {
+                if(!res.ok){
+                    throw Error('Could not fetch the data for that resource')
+                }
                 return res.json();
             })
             .then(data => {
                 setBlogs(data);
                 setPending(false);
+                setError(null);
+            })
+            .catch(err => {
+                setPending(false);
+                setError(err.message);
             });
     });
 
@@ -33,6 +42,7 @@ const Home = () => {
 
     return (
         <div className="Home">
+            {error && <div>{ error }</div>}
             {pending && <div>loading...</div>}
             {blogs && <BlogList blogs={blogs} title="All Blogs" />}
         </div>
