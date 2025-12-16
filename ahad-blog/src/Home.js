71e9d560937ee35832c74e95076989c86f53bb66
Ahad Danish <ahad@Ahads-MacBook-Pro.local>
2025-12-16 13:15:47 +0500

    onClick events

--- ahad-blog/src/Home.js
+++ ahad-blog/src/Home.js
@@ -1,7 +1,31 @@
 const Home = () => {
+    const handleClick = () => {
+        console.log("Hello Ahad");
+    }
+    const handleClickTarget = (e) => {
+        console.log("Hello Ahad", e);
+    }
+
+    const handleClickName = (name) => {
+        console.log("Name: Hello " + name);
+    }
+ 
+    const handleClickNameTarget = (name,e) => {
+        console.log("Name: Hello " + name, e);
+    }
+
     return ( 
         <div className="Home">
             <h2>HomePage</h2>
+
+            <button onClick={handleClick()}>Click Me</button> ### Will render automatically on load <br/>
+            <button onClick={handleClick}>Click Me</button> ### Correct Way: will render on click <br/>
+            <button onClick={handleClickTarget}>Click Me</button> ### Correct Way: will render on click with event properties e <br/>
+            <button onClick={handleClickName("Ahad")}>Click Me</button> ### Will render automatically on load <br/>
+            <button onClick={() => handleClickName("Ahad")}>Click Me</button> ### Correct Way: will render on click <br/>
+            <button onClick={(e) => handleClickNameTarget("Ahad", e)}>Click Me</button> ### Correct Way: will render on click with event properties e <br/>
+
+            ### Note: Auto execution function on load with console two times due to StrictMode used in index.js
         </div> 
     );
 }
