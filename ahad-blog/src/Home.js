9709fc3ba0636db17d6f9c0ef12791fdd314ad9b
Ahad Danish <ahad@Ahads-MacBook-Pro.local>
2025-12-16 15:50:38 +0500

    Simple useEffect: it runs whenever any state is changed or rendered i.e on reload, on useState(setBlog)

--- ahad-blog/src/Home.js
+++ ahad-blog/src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
@@ -13,6 +13,11 @@ const Home = () => {
         const newBlog = blogs.filter((blog) => blog.id !== id);
         setBlogs(newBlog);
     }
+
+    useEffect(()=>{
+        console.log("useEffect ran");
+    })
+    
     return (
         <div className="Home">
             <BlogList blogs = {blogs} title = "All Blogs" handleDelete = {handleDelete} />
