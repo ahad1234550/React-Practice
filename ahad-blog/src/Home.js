95772925c309f0a6bf24d339fcd38b511225ab3d
Ahad Danish <ahad@Ahads-MacBook-Pro.local>
2025-12-16 16:48:20 +0500

    Added db.json as database and created endpoint api for this database instructions in Readme.md

--- ahad-blog/src/Home.js
+++ ahad-blog/src/Home.js
@@ -3,12 +3,6 @@ import BlogList from "./BlogList";
 
 const Home = () => {
 
-    const [name, setName] = useState("ahad");
-
-    const handleClick = () => {
-        setName("Ahad");
-    }
-
     const [blogs, setBlogs] = useState([
         { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
         { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
@@ -22,13 +16,11 @@ const Home = () => {
 
     useEffect(()=>{
         console.log("useEffect ran");
-    },[blogs]) // if there are so many states i.e blogs, age, name etc and you want to use on blogs and age both simple write comma separated [blogs,age]
+    })
 
     return (
         <div className="Home">
             <BlogList blogs = {blogs} title = "All Blogs" handleDelete = {handleDelete} />
-            <h3>Name: { name }</h3>
-            <button onClick={handleClick}>Correct Name</button>
         </div>
     );
 }
