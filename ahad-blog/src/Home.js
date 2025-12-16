1a2843031f9ccb99e719c1080c1e9bb4765daed5
Ahad Danish <ahad@Ahads-MacBook-Pro.local>
2025-12-16 18:58:39 +0500

    Added react router to make the routes for home and create page in App.js

--- ahad-blog/src/Home.js
+++ ahad-blog/src/Home.js
@@ -3,11 +3,11 @@ import useFetch from "./useFetch";
 
 const Home = () => {
 
-    const {data : blogs, pending, error} = useFetch("http://localhost:8000/blogs")
+    const { data: blogs, pending, error } = useFetch("http://localhost:8000/blogs")
 
     return (
         <div className="Home">
-            {error && <div>{ error }</div>}
+            {error && <div>{error}</div>}
             {pending && <div>loading...</div>}
             {blogs && <BlogList blogs={blogs} title="All Blogs" />}
         </div>
