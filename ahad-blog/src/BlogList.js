b9d6dfc56b45710e35524ee1d7261a836b4a669c
Ahad Danish <ahad@Ahads-MacBook-Pro.local>
2025-12-16 19:31:27 +0500

    Create BlogDetail.js and its route to show its id on the page through url param and added that route Link to BlogList.js

--- ahad-blog/src/BlogList.js
+++ ahad-blog/src/BlogList.js
@@ -1,9 +1,5 @@
-// Calling using props
-// const BlogList = (props) => {
-//     const blogs = props.blogs;
-//     const title = props.title;
+import { Link } from "react-router-dom";
 
-// Calling directly
 const BlogList = ({blogs, title}) => {
 
     return ( 
@@ -11,8 +7,10 @@ const BlogList = ({blogs, title}) => {
             <h2>{ title }</h2>
             {blogs.map(blog => (
                 <div className="blog-preview" key={blog.id} >
+                    <Link to={`/blog/${blog.id}`}>
                     <h2>{blog.title}</h2>
                     <p>Written by {blog.author}</p>
+                    </Link>
                 </div>
             ))}
         </div>
