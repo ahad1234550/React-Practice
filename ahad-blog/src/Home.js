5347e849bd536ce024bc3556c721829d3557e1fa
Ahad Danish <ahad@Ahads-MacBook-Pro.local>
2025-12-16 18:18:45 +0500

    separated the useEffect in useFetch.js that is fetching data from parameter url to reuse

--- ahad-blog/src/Home.js
+++ ahad-blog/src/Home.js
@@ -1,44 +1,9 @@
-import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
 
-    const [blogs, setBlogs] = useState(null);
-    const [pending, setPending] = useState(true);
-    const [error, setError] = useState(null);
-
-    useEffect(() => {
-        fetch("http://localhost:8000/blogs")
-            .then(res => {
-                if(!res.ok){
-                    throw Error('Could not fetch the data for that resource')
-                }
-                return res.json();
-            })
-            .then(data => {
-                setBlogs(data);
-                setPending(false);
-                setError(null);
-            })
-            .catch(err => {
-                setPending(false);
-                setError(err.message);
-            });
-    });
-
-    // use this as it will fetch the data after 1 second so that you can see loading...
-    // useEffect(() => {
-    //     setTimeout(() => {
-    //         fetch("http://localhost:8000/blogs")
-    //             .then(res => {
-    //                 return res.json();
-    //             })
-    //             .then(data => {
-    //                 setBlogs(data);
-    //                 setPending(false);
-    //             });
-    //     }, 1000);
-    // });
+    const {data : blogs, pending, error} = useFetch("http://localhost:8000/blogs")
 
     return (
         <div className="Home">
---
+++ ahad-blog/src/useFetch.js
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+
+
+const useFetch = (url) => {
+
+    const [data, setData] = useState(null);
+    const [pending, setPending] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        fetch(url)
+            .then(res => {
+                if (!res.ok) {
+                    throw Error('Could not fetch the data for that resource')
+                }
+                return res.json();
+            })
+            .then(data => {
+                setData(data);
+                setPending(false);
+                setError(null);
+            })
+            .catch(err => {
+                setPending(false);
+                setError(err.message);
+            });
+    },[url]);
+    return {data, pending, error};
+}
+
+export default useFetch;
