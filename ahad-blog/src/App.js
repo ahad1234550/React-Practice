0ed971ba9773fac921da53a87e7f26c1af02ef19
Ahad Danish <ahad@Ahads-MacBook-Pro.local>
2025-12-17 12:24:36 +0500

    Added 404 page and its route in App.js

--- ahad-blog/src/App.js
+++ ahad-blog/src/App.js
@@ -3,6 +3,7 @@ import Home from './Home';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Create from './create';
 import BlogDetail from './BlogDetail';
+import NotFound from './NotFound';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/create" element={<Create />} />
             <Route path="/blog/:id" element={<BlogDetail />} />
+            <Route path='*' element={<NotFound />} />
           </Routes>
         </div>
       </div>
