8c4487051446ddec5ddee08bfe85c12ea7e3405a
Ahad Danish <ahad@Ahads-MacBook-Pro.local>
2025-12-17 11:38:57 +0500

    Added form to create a new blog and trigger the values to onscreen on onChange using useState

--- ahad-blog/src/create.js
+++ ahad-blog/src/create.js
@@ -1,7 +1,30 @@
+import { useState } from "react";
+
 const Create = () => {
+
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('Ahad');
+
     return (
         <div className="create">
             <h2>Add New Blog</h2>
+            <form>
+                <label>Blog Title</label>
+                <input type="text" required value={ title } onChange={(e) => { setTitle(e.target.value) }}/>
+
+                <label>Blog Body</label>
+                <textarea required value={ body } onChange={(e) => { setBody(e.target.value) }}/>
+
+                <label>Blog Author</label>
+                <select required value={ author } onChange={(e) => { setAuthor(e.target.value) }}>
+                    <option value="Ahad">Ahad</option>
+                    <option value="Fahad">Fahad</option>
+                </select>
+            </form>
+            <p>{ title }</p>
+            <p>{ body }</p>
+            <p>{ author }</p>
         </div>
     );
 }
